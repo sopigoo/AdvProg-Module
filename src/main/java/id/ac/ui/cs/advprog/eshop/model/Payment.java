a5e895a13b96bb7566e1f6c2ac413385ccbf08ea
sopigoo <shofinadhifa24@gmail.com>
2025-03-07 11:47:02 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,22 +1,44 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
 import lombok.Getter;
-import lombok.Setter;
-
 import java.util.Map;
+import java.util.Arrays;
 
 @Getter
 public class Payment {
     String id;
     String method;
-
-    @Setter
     String status;
     Map<String, String> paymentData;
 
     public Payment(String id, String method, Map<String, String> paymentData) {
+        this.id = id;
+        this.method = method;
+        this.status = "WAITING";
+
+        if (paymentData == null) {
+            throw new NullPointerException("Payment data cannot be null");
+        } else {
+            this.paymentData = paymentData;
+        }
     }
 
     public Payment(String id, String method, String status, Map<String, String> paymentData) {
+        this(id, method, paymentData);
+
+        String[] statusList = {"WAITING", "SUCCESS", "FAILED"};
+        if (Arrays.stream(statusList).noneMatch(item -> item.equals(status))) {
+            throw new IllegalArgumentException("Invalid status");
+        } else {
+            this.status = status;
+        }
+    }
+
+    public void setStatus(String status) {
+        String[] statusList = {"WAITING", "SUCCESS", "FAILED"};
+        if (Arrays.stream(statusList).noneMatch(item -> item.equals(status))) {
+            throw new IllegalArgumentException("Invalid status");
+        }
+        this.status = status;
     }
 }
