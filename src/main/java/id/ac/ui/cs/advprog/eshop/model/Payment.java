3055bfbe1f9fd73817302b9335bf21b250408968
sopigoo <shofinadhifa24@gmail.com>
2025-03-07 12:01:25 +0700

    [REFACTOR] Apply PaymentStatus enum check to Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,8 +1,8 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import lombok.Getter;
 import java.util.Map;
-import java.util.Arrays;
 
 @Getter
 public class Payment {
@@ -14,31 +14,24 @@ public class Payment {
     public Payment(String id, String method, Map<String, String> paymentData) {
         this.id = id;
         this.method = method;
-        this.status = "WAITING";
+        this.status = PaymentStatus.WAITING.getValue();
 
         if (paymentData == null) {
             throw new NullPointerException("Payment data cannot be null");
-        } else {
-            this.paymentData = paymentData;
         }
+        this.paymentData = paymentData;
     }
 
     public Payment(String id, String method, String status, Map<String, String> paymentData) {
         this(id, method, paymentData);
-
-        String[] statusList = {"WAITING", "SUCCESS", "FAILED"};
-        if (Arrays.stream(statusList).noneMatch(item -> item.equals(status))) {
-            throw new IllegalArgumentException("Invalid status");
-        } else {
-            this.status = status;
-        }
+        this.setStatus(status);
     }
 
     public void setStatus(String status) {
-        String[] statusList = {"WAITING", "SUCCESS", "FAILED"};
-        if (Arrays.stream(statusList).noneMatch(item -> item.equals(status))) {
+        if (PaymentStatus.contains(status)) {
+            this.status = status;
+        } else {
             throw new IllegalArgumentException("Invalid status");
         }
-        this.status = status;
     }
 }
